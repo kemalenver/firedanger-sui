5240ed63b1f5f92eb802a954d00f89f25c1db3d6
Kemal Enver <kemalenver@gmail.com>
2022-01-01 16:10:18 +1100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kemal Enver on 1/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/firedanger_suiApp.swift
@@ -0,0 +1,17 @@
+//
+//  firedanger_suiApp.swift
+//  Shared
+//
+//  Created by Kemal Enver on 1/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct firedanger_suiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
