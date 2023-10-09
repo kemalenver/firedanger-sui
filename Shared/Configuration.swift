8699db18809295edebf707c0d651d60856bd97dc
Kemal Enver <kemalenver@gmail.com>
2023-10-09 15:26:25 +1100

    Include release ids

--- Shared/Configuration.swift
+++ Shared/Configuration.swift
@@ -11,7 +11,7 @@ struct Configuration {
     static let fireDangerRatingURL = "https://www.rfs.nsw.gov.au/plan-and-prepare/fire-danger-ratings"
     static let rulesURL = "https://www.rfs.nsw.gov.au/fire-information/fdr-and-tobans/total-fire-ban-rules"
     static let advertWidth: CGFloat = 320
-    static private let releaseBannerID = "ca-app-pub-enter-id-here"
+    static private let releaseBannerID = "ca-app-pub-6219860372037158/5629745135"
     static private let debugBannerID = "ca-app-pub-3940256099942544/2435281174"
     
     static func bannerID() -> String {
